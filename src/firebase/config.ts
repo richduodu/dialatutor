9976730156ef43d4c4b198d7e1241fd510111ebf
Richard Duodu <richduodu@gmail.com>
2026-03-08 20:52:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2209975741-b9097",
+  "appId": "1:94020746016:web:2c3d95b4e6e5d9922ef545",
+  "apiKey": "AIzaSyC81aJRo6ZxKSu_LTc0RC6rUjX3BUuZm1Q",
+  "authDomain": "studio-2209975741-b9097.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "94020746016"
+};
