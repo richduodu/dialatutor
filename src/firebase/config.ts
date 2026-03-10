6c461ea2d6e89c1c6690a78e139ce33c070e3ad7
Richard Duodu <richduodu@gmail.com>
2026-03-10 18:28:31 +0000

    the Google API Key is leaked on github. update it to: AIzaSyAxsVTWs8iXSB

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2209975741-b9097",
   "appId": "1:94020746016:web:2c3d95b4e6e5d9922ef545",
-  "apiKey": "AIzaSyC81aJRo6ZxKSu_LTc0RC6rUjX3BUuZm1Q",
+  "apiKey": "AIzaSyAxsVTWs8iXSBGAuo2u7trxjLAbO1FPmrg",
   "authDomain": "studio-2209975741-b9097.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "94020746016"
